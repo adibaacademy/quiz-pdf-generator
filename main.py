7da708e28b8ddc3c1e86e25bc2de0c5cb3ad0709
adibaacademy <adibaacademy@gmail.com>
2026-07-11 13:23:38 +0600

    Add files via upload

--- main.py
+++ main.py
@@ -32,7 +32,8 @@ def get_gemini_content(data):
     """
 
     api_key = os.getenv("GEMINI_API_KEY")
-    url = f"[https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=](https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=){api_key}"
+
+    url = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key={api_key}"
     
     payload = {"contents": [{"parts": [{"text": prompt}]}]}
     response = requests.post(url, json=payload)
